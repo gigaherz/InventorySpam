9e1a7dd35e2aea5e293816d901afaee166247be0
David Quintana <gigaherz@gmail.com>
2019-11-27 21:30:22 +0100

    Fix mod causing the server list to show as incompatible, even though the mod is client-only.

--- src/main/java/gigaherz/inventoryspam/InventorySpam.java
+++ src/main/java/gigaherz/inventoryspam/InventorySpam.java
@@ -1,11 +1,14 @@
 package gigaherz.inventoryspam;
 
 import gigaherz.inventoryspam.config.ConfigData;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(InventorySpam.MODID)
 public class InventorySpam
@@ -22,6 +25,9 @@ public class InventorySpam
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::bakeConfigs);
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigData.CLIENT_SPEC);
+
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 
         //ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (mc, screen) -> ConfigGuiFactory.createConfigGui(mc, screen));
     }
